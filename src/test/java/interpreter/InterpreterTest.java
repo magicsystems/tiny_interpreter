aa0935e9cea78ccc068782c2791ba4e411f6fd36
Andrey Kudryavtsev <andrey.kudryavtsev@kohls.com>
2017-02-19 20:35:28 +0300

    Formatting for InterpreterTest

--- src/test/java/interpreter/InterpreterTest.java
+++ src/test/java/interpreter/InterpreterTest.java
@@ -12,14 +12,25 @@ import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertThat;
 
 public class InterpreterTest {
+
     private static final String PROGRAM_ONE =
             "var n = 5\n" +
                     "var sequence = map({1, n}, i -> 2^i + 1)\n" +
                     "var val = reduce(sequence, 0, x y -> x + y)\n" +
                     "print \"val = \"\n" +
                     "out val";
+    @Test
+    public void testInterpretation() {
+        runProgram(PROGRAM_ONE, Arrays.asList("val = ", "67.0"));
+    }
+
 
     private static final String HELLO_WORLD = "print \"Hello World!\"";
+
+    @Test
+    public void testHelloWorld() {
+        runProgram(HELLO_WORLD, Arrays.asList("Hello World!"));
+    }
 
     private static final String PRINT_SEQUENCE =
             "var sequence = map({1, 4}, i -> (-1)^(i+1))\n" +
@@ -28,39 +39,6 @@ public class InterpreterTest {
     private static final String PRINT_SEQUENCE_VARIABLE =
             "var sequence = {1,4}\n" +
                     "out sequence";
-
-    private static final String INVALID_TYPE_REDUCE_PROGRAM =
-            "var n = 5\n" +
-                    "var val = reduce(n, 0, x y -> x + y)\n" +
-                    "print \"val = \"\n" +
-                    "out val";
-
-    private static final String INVALID_TYPE_MAP_PROGRAM =
-            "var n = {1,5}\n" +
-                    "var red = reduce(n, 0, x y -> x + y)\n" +
-                    "var val = map(red, y -> 3 + y)\n";
-
-
-    private static final String TWO_REDUCE_PROGRAM =
-            "var m = map({1,7}, x -> x/2)\n" +
-                    "var sum = reduce(m, 0, x y -> x + y)\n" +
-                    "var mult = reduce(m, 1, x y -> x * y)\n" +
-                    "out sum\n" +
-                    "out mult\n";
-
-    private static final String DUPLICATE_IDENTIFIERS_PROGRAM =
-            "var m = map({1,7}, x -> x/2)\n" +
-                    "var m = reduce(m, 0, x y -> x + y)\n";
-
-    @Test
-    public void testInterpretation() {
-        runProgram(PROGRAM_ONE, Arrays.asList("val = ", "67.0"));
-    }
-
-    @Test
-    public void testHelloWorld() {
-        runProgram(HELLO_WORLD, Arrays.asList("Hello World!"));
-    }
 
     @Test
     public void testPrintSequence() {
@@ -72,6 +50,12 @@ public class InterpreterTest {
         runProgram(PRINT_SEQUENCE_VARIABLE, Arrays.asList("{1.0, 2.0, 3.0, 4.0}"));
     }
 
+    private static final String INVALID_TYPE_REDUCE_PROGRAM =
+            "var n = 5\n" +
+                    "var val = reduce(n, 0, x y -> x + y)\n" +
+                    "print \"val = \"\n" +
+                    "out val";
+
     @Test
     public void testInvalidTypeReduceProgram() {
         runProgramWithError(INVALID_TYPE_REDUCE_PROGRAM,
@@ -79,16 +63,32 @@ public class InterpreterTest {
                 "Undefined variable 'val'");
     }
 
+    private static final String INVALID_TYPE_MAP_PROGRAM =
+            "var n = {1,5}\n" +
+                    "var red = reduce(n, 0, x y -> x + y)\n" +
+                    "var val = map(red, y -> 3 + y)\n";
+
     @Test
     public void testInvalidTypeMapProgram() {
         runProgramWithError(INVALID_TYPE_MAP_PROGRAM,
                 "Incompatible type error. Sequence is required, but got " + "'red'");
     }
 
+    private static final String TWO_REDUCE_OPERATION_PROGRAM =
+            "var m = map({1,7}, x -> x/2)\n" +
+                    "var sum = reduce(m, 0, x y -> x + y)\n" +
+                    "var mult = reduce(m, 1, x y -> x * y)\n" +
+                    "out sum\n" +
+                    "out mult\n";
+
     @Test
     public void testTwoReduce() {
-        runProgram(TWO_REDUCE_PROGRAM, Arrays.asList("14.0", "39.375"));
+        runProgram(TWO_REDUCE_OPERATION_PROGRAM, Arrays.asList("14.0", "39.375"));
     }
+
+    private static final String DUPLICATE_IDENTIFIERS_PROGRAM =
+            "var m = map({1,7}, x -> x/2)\n" +
+                    "var m = reduce(m, 0, x y -> x + y)\n";
 
     @Test
     public void testDuplicate() {
